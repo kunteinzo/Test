1bc550919d16a97d5e2cf8654809d632b68211ff
Khun Thein Htet Oo <theinhtetoo473@gmail.com>
2025-04-08 11:22:17 +0630

    resolve compiling Kotlin with lib

--- kotlin/b.sh
+++ kotlin/b.sh
@@ -4,13 +4,16 @@ root=~/Projects/Test/kotlin
 rm $root/app.jar > /dev/null 2>&1
 
 shopt -s globstar
-kotlinc $root/src/**/*.kt -include-runtime -d $root/app.jar
+kotlinc $root/src/**/*.kt -include-runtime -cp $(ls -d $root/lib/*.jar | tr "\n" ":") -d $root/classes -Xallow-no-source-files
+
 shopt -u globstar
 if [ $? -ne 0 ];then
 	echo
 	echo "Failed to compile Kotlin app!"
 	exit 1
 fi
+
+jar cfm $root/app.jar $root/manifest.txt -C $root/classes . -C $root/lib .
 
 echo
 kotlin $root/app.jar
--- kotlin/src/com/mykotlin/Main.kt
+++ kotlin/src/com/mykotlin/Main.kt
@@ -1,5 +1,8 @@
 package com.mykotlin
 
+import com.google.gson.Gson
+
 fun main() {
 	println("Hello world")
+	println(Gson().toJson(arrayOf("Hi", "Hello")))
 }
