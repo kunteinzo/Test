7c18df648dd9ccd2874a2d415ecaf4bba24576d8
Khun Thein Htet Oo <theinhtetoo473@gmail.com>
2025-04-08 12:17:30 +0630

    resolve compiling Java with lib

--- java/b.sh
+++ java/b.sh
@@ -5,7 +5,7 @@ root=~/Projects/Test/java
 rm -rf $root/app.jar $root/classes > /dev/null 2>&1
 
 shopt -s globstar
-javac -d $root/classes $root/src/**/*.java
+javac -d $root/classes -cp $(ls -d $root/lib/*.jar | tr "\n" ":") $root/src/**/*.java
 shopt -u globstar
 
 jar cfm $root/app.jar $root/manifest.txt -C $root/classes .
--- java/src/com/myjava/Main.java
+++ java/src/com/myjava/Main.java
@@ -1,9 +1,12 @@
 package com.myjava;
 
+import com.google.gson.Gson;
+
 class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println(new Gson().toJson(new String[] {"hi", "hello"}));
 	}
 
 }
