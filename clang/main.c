5016868e304995394214e4e5d0b46e2b2ec184b1
Khun Thein Htet Oo <theinhtetoo473@gmail.com>
2025-04-10 16:39:14 +0630

    edit c

--- clang/main.c
+++ clang/main.c
@@ -2,7 +2,13 @@
 #include <stdbool.h>
 
 int main() {
+	int age = 0;
+	age = 21;
 	printf("Hello World!\n");
 	printf("This is my C app.\n");
+	printf("I am %d years old.\n", age);
+	printf("Please enter your age: ");
+	scanf("%d", &age);
+	printf("I am %d years old.\n", age);
 	return 0;
 }
