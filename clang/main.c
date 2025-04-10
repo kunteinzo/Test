23781b52410e5bc2a913d90dd500a7a0b0e134f6
Khun Thein Htet Oo <theinhtetoo473@gmail.com>
2025-04-10 16:23:00 +0630

    Edit c

--- clang/main.c
+++ clang/main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
 	printf("Hello World!\n");
-	printf("This is my C app.");
+	printf("This is my C app.\n");
+	bool areYouGay = true;
+	printf("Are you gay?: ");
+	scanf("%d", &areYouGay);
+	if (areYouGay)
+	{
+		printf("The user is gay.\n");
+	} else
+	{
+		printf("The user isn't gay.");
+	}
+	
+	
 	return 0;
 }
