28291a653726cadc2ad346b7e2962d0b3826070f
Khun Thein Htet Oo <theinhtetoo473@gmail.com>
2025-04-10 16:48:11 +0630

    edit c++

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main() {
+	const int MAX = 100;
+	string name;
+	int age;
+	cout << "Enter your name: ";
+	getline(cin, name); // Read a line of text
+	cout << "Enter an integer: ";
+	cin >> age; // Do not skip whitespace
+	cout << "Hello " << name << ", you are " << age << " years old.\n" << endl;
 	cout << "Hello World!\n";
 	cout << "This is my C++ app.";
  	return 0;
