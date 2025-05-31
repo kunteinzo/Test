c370a54282eda32e7075539db5152a1a7ffdc3a8
Khun Thein Htet Oo <theinhtetoo473@gmail.com>
2025-05-31 11:21:46 +0630

    u

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const postgres_1 = __importDefault(require("postgres"));
+const sql = (0, postgres_1.default)({
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    ssl: 'require',
+});
