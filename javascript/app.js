64a721be53f3dae1fc3b4dd2219593eedbfb365d
Khun Thein Htet Oo <theinhtetoo473@gmail.com>
2025-05-31 07:50:49 +0630

    testing node ts

--- javascript/app.js
+++
@@ -1,7 +0,0 @@
-console.log('This is JavaScript');
-
-function main() {
-    console.log("This is my main function.");
-}
-
-main();
---
+++ javascript/index.ts
@@ -0,0 +1,13 @@
+import postgres from 'postgres';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const sql = postgres({
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  ssl: 'require',
+});
